6e081ebf192508d464fb22441954c0a88145c773
Yona-TYT <yonatan.el.amigo@gmail.com>
2022-09-19 19:46:18 -0400

    test valores menores a cero

--- resources/js/main.js
+++ resources/js/main.js
@@ -242,7 +242,7 @@ function soltar_tecla(e){
 }
 
 function remplace_test(num) {
-	//if(gl_browser){
+	if(gl_browser){
 		alert(get_dignr(num))
 		//num = num.replace(/(\.)(\d){2,}/g, 128);
 		if(get_dignr(num) == 0)
@@ -252,7 +252,18 @@ function remplace_test(num) {
 		 //num.replace(/\.$/, "128");
 		// num.replace(/[\.]$/, 128);
 		//add_message(num);
-	//}
+	}
+	else{
+		alert(get_dignr(num))
+		//num = num.replace(/(\.)(\d){2,}/g, 128);
+		if(get_dignr(num) == 0)
+			num = num.replace(/($)/g, ",00");
+
+		else num = num+",00"
+		 //num.replace(/\.$/, "128");
+		// num.replace(/[\.]$/, 128);
+		//add_message(num);
+	}
 	return num;
 }
 
